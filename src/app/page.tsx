e308a45b0d1701f92c3497f48ed81a524d456253
Nimira43 <n.rabone72@aol.com>
2025-08-06 18:59:21 +0100

    HomePage -3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ const HomePage = () => {
       />
       <h1 className='text-4xl md:text-5xl font-medium text-support logo mb-4'>Modulas</h1>
       <p className='text-lg text-light mb-8 uppercase'>Support Ticket Management System</p>
-      <div className='flex flex-col md:flex-row gap-4 justify-center'>
+      <div className='flex flex-col md:flex-row gap-4 justify-center animate-slide opacity-0'>
         <Link
           href='tickets/new'
           className='bg-light text-main-dark uppercase font-medium px-6 py-3 rounded hover:bg-support btn-hover'
