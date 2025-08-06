cd009ab65f8c96f5e2ab5ab6eedfbc63069e0a0c
Nimira43 <n.rabone72@aol.com>
2025-08-06 16:48:57 +0100

    Tailwind custom colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,22 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'main': '#008080',
+        'main-dark': '#012222',
+        'main-light': '#3ee4e4',
+        'main-light-extra': '#91e8e8',
+        'support': '#ffb700',
+        'support-light': '#f8df9f',
+        'dark': '#111',
+        'light': '#eeffff',
+        'grey-dark': '#333',
+        'grey-medium': '#999',
+        'grey-light': '#ccc',
+        'grey-light-extra': '#eee',
+      }
+    },
   },
   plugins: [],
 }
