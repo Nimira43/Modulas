30e25045f2e85622fbad03753d32400e7b76e043
Nimira43 <n.rabone72@aol.com>
2025-08-06 19:15:34 +0100

    NewTicketPage - 2

--- src/app/tickets/new/page.tsx
+++ src/app/tickets/new/page.tsx
@@ -2,7 +2,10 @@ const NewTicketPage = () => {
   return ( 
     <div className='min-h-screen bg-main-dark flex items-center justify-center px-4'>
       <div className='w-full max-wi-md bg-light rounded-md p-8'>
-        
+        <h1 className='text-3xl font-medium mb-6 text-center text-main-dark uppercase'>Submit A Support Ticket</h1>
+        <form className='space-y-4 text-dark'>
+          
+        </form>
       </div>
     </div>
    )
