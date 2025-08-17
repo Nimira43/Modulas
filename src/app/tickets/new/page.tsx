8aeac654eb618a6ef36d94c24c6987ed9ff1885e
Nimira43 <n.rabone72@aol.com>
2025-08-17 11:18:52 +0100

    Subject input field

--- src/app/tickets/new/page.tsx
+++ src/app/tickets/new/page.tsx
@@ -4,7 +4,13 @@ const NewTicketPage = () => {
       <div className='w-full max-wi-md bg-light rounded-md p-8'>
         <h1 className='text-3xl font-medium mb-6 text-center text-main-dark uppercase'>Submit A Support Ticket</h1>
         <form className='space-y-4 text-dark'>
-          
+          <input 
+            className='w-full border border-grey-medium p-3 rounded focus:outline-none' 
+            type='text'
+            name='subject'
+            placeholder='Subject'
+            required 
+          />
         </form>
       </div>
     </div>
