59234df7f66109eb293a7a51b01c9740ddce185d
Nimira43 <n.rabone72@aol.com>
2025-08-25 23:56:56 +0100

    Creating variables for form fields

--- src/actions/ticket.actions.ts
+++ src/actions/ticket.actions.ts
@@ -1,5 +1,7 @@
 'use server'
 
 export async function createTicket(formData: FormData) {
-  
+  const subject = formData.get('subject') as string
+  const description = formData.get('description') as string
+  const priority = formData.get('priority') as string
 }
