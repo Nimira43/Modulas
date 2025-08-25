294dc1abea7dc426534b5c008a71bc1181994c18
Nimira43 <n.rabone72@aol.com>
2025-08-26 00:02:42 +0100

    Able to submit tick to console.

--- src/app/tickets/new/page.tsx
+++ src/app/tickets/new/page.tsx
@@ -1,11 +1,14 @@
-import { createTicket } from "@/actions/ticket.actions"
+import { createTicket } from '@/actions/ticket.actions'
 
 const NewTicketPage = () => {
   return ( 
     <div className='min-h-screen bg-main-dark flex items-center justify-center px-4'>
       <div className='w-full max-wi-md bg-light rounded-md p-8'>
         <h1 className='text-3xl font-medium mb-6 text-center text-dark'>Submit A Support Ticket</h1>
-        <form className='space-y-4 text-dark'>
+        <form 
+          action={createTicket}
+          className='space-y-4 text-dark'
+        >
           <input 
             className='w-full border border-grey-medium p-3 rounded focus:outline-none' 
             type='text'
