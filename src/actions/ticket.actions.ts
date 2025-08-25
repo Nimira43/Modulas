85e925e74101349a1fe96b1fbcbdbc623ddabf97
Nimira43 <n.rabone72@aol.com>
2025-08-26 00:08:25 +0100

    Added prevState parameter into createTicket function

--- src/actions/ticket.actions.ts
+++ src/actions/ticket.actions.ts
@@ -1,6 +1,6 @@
 'use server'
 
-export async function createTicket(formData: FormData) {
+export async function createTicket(prevState, formData: FormData) {
   const subject = formData.get('subject') as string
   const description = formData.get('description') as string
   const priority = formData.get('priority') as string
