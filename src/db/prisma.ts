5c50981c35f5fdb46c3df818b8eb77dd588b262e
Nimira43 <n.rabone72@aol.com>
2025-09-21 12:53:12 +0100

    prisma

--- src/db/prisma.ts
+++ src/db/prisma.ts
@@ -3,3 +3,6 @@ import { PrismaClient } from '@/generated/prisma'
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
+
+export const prisma = globalForPrisma.prisma ?? new PrismaClient()
+
