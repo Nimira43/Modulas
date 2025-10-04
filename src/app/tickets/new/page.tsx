82ee1f70593aea77e44e46b6e00cc0bfa4ebef7e
Nimira43 <n.rabone72@aol.com>
2025-10-04 09:33:29 +0100

    Importing useEffect into NewTicketPage

--- src/app/tickets/new/page.tsx
+++ src/app/tickets/new/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useActionState } from 'react'
+import { useActionState, } from 'react'
 import { createTicket } from '@/actions/ticket.actions'
 
 const NewTicketPage = () => {
