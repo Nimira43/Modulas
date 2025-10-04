e117a806615b51a6e2732d5124fdbcb9d6330909
Nimira43 <n.rabone72@aol.com>
2025-10-04 07:58:48 +0100

    logEvent function with params and types

--- src/utils/sentry.ts
+++ src/utils/sentry.ts
@@ -2,3 +2,10 @@ import * as Sentry from '@sentry/nextjs'
 
 type LogLevel = 'fatal' | 'error' | 'warning' | 'info' | 'debug'
 
+export function logEvent(
+  message: string,
+  category: string = 'general',
+  data?: Record<string, any>,  
+  logLevel: LogLevel = 'info',
+  error?: unknown
+) {}
