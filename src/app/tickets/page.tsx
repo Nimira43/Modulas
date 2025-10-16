cc91e6d89f5b74183f9c2581ad49035d85151d53
Nimira43 <n.rabone72@aol.com>
2025-10-16 18:09:01 +0100

    TicketsPage output - 2

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -8,7 +8,13 @@ const TicketsPage = async () => {
   return ( 
     <div className='min-h-screen bg-light p-8'>
       <h1 className='text-3xl font-medium text-main-dark text-center mb-8'>Support Tickets</h1>
-      
+      {tickets.length === 0 ? (
+        <p className='text-center text-grey-dark'>No tickets to show.</p>
+      ) : (
+        <div className='space-y-4 max-w-3xl'>
+          <p>Tickets</p>
+        </div>
+      )}
     </div>
    )
 }
