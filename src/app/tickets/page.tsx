1bf800a250296ae7a36e2f683f8e47751c53d355
Nimira43 <n.rabone72@aol.com>
2025-10-16 19:00:54 +0100

    getPriorityClass function

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -2,6 +2,17 @@ import { getTickets } from '@/actions/ticket.actions'
 import { logEvent } from '@/utils/sentry'
 import Link from 'next/link'
 
+const getPriorityClass = (priority: string) => {
+  switch(priority) {
+    case 'High':
+      return 'text-red-600 font-medium' 
+    case 'Medium':
+      return 'text-amber-600 font-medium' 
+    case 'Low':
+      return 'text-green-600 font-medium' 
+  }
+}
+
 const TicketsPage = async () => {
   const tickets = await getTickets()
   
