2677caa24a4debf2d8673d911c51f9f7612860f5
Nimira43 <n.rabone72@aol.com>
2025-10-16 20:04:39 +0100

    Imported getTicketById, logEvent, Link and notFound

--- src/app/tickets/[id]/page.tsx
+++ src/app/tickets/[id]/page.tsx
@@ -1,3 +1,8 @@
+import { getTicketById } from '@/actions/ticket.actions'
+import { logEvent } from '@/utils/sentry'
+import Link from 'next/link'
+import { notFound } from 'next/navigation'
+
 const TicketDetailsPage = () => {
   return ( 
     <>
