a068ab9fe522922a677d43ff0ac47818bf432b2b
Nimira43 <n.rabone72@aol.com>
2025-10-16 17:55:49 +0100

    Create tickets with getTickets - console logging tickets

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -2,7 +2,10 @@ import { getTickets } from '@/actions/ticket.actions'
 import { logEvent } from '@/utils/sentry'
 import Link from 'next/link'
 
-const TicketsPage = () => {
+const TicketsPage = async () => {
+  const tickets = await getTickets()
+  console.log(tickets)
+
   return ( 
     <>
       Tickets Page
