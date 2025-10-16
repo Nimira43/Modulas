b7cf5f2bdf904bea137ea8284c2d95751ed65052
Nimira43 <n.rabone72@aol.com>
2025-10-16 18:33:22 +0100

    TicketPage output - 5

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -11,14 +11,24 @@ const TicketsPage = async () => {
       {tickets.length === 0 ? (
         <p className='text-center text-light'>No tickets to show.</p>
       ) : (
-        <div className='space-y-4 max-w-3xl'>
+        <div className='space-y-4 max-w-3xl mx-auto'>
           { tickets.map((ticket) => (
             <div 
               key={ticket.id}
               className='flex justify-between item-center bg-light rounded-lg shadow p-6'
             >
-              <h2 className='text-xl font-medium text-main-dark'>{ticket.subject}</h2>
+              <div>
+                <h2 className='text-xl font-medium text-main-dark'>{ticket.subject}</h2>
+              </div>
+              <div className='text-right space-y-2'>
+                <div className='text-sm text-dark'>
+                  Priority: <span>
+                    {ticket.priority}
+                  </span>
+                </div>
+              </div>
             </div>
+
           ))}
         </div>
       )}
