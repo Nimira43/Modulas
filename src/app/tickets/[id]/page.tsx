5bdf26e3368725e670d3aaf8ffd292e9d7c709dc
Nimira43 <n.rabone72@aol.com>
2025-10-16 20:07:20 +0100

    Copied over the getPriorityClass function

--- src/app/tickets/[id]/page.tsx
+++ src/app/tickets/[id]/page.tsx
@@ -3,11 +3,22 @@ import { logEvent } from '@/utils/sentry'
 import Link from 'next/link'
 import { notFound } from 'next/navigation'
 
+const getPriorityClass = (priority: string) => {
+  switch(priority) {
+    case 'High':
+      return 'text-red-600 font-medium' 
+    case 'Medium':
+      return 'text-amber-600 font-medium' 
+    case 'Low':
+      return 'text-green-600 font-medium' 
+  }
+}
+
 const TicketDetailsPage = () => {
   return ( 
-    <>
+    <div className='min-h-screen bg-main-dark p-8'>
       <h1>Ticket Details Page</h1>
-    </> 
+    </div> 
   )
 }
 
