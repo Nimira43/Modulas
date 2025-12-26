78bd756dd7941fe07e141b28891f1e1e8bbc780a
Nimira43 <n.rabone72@aol.com>
2025-12-26 06:00:31 +0000

    Import registerUser

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -3,6 +3,7 @@
 import { useActionState, useEffect } from 'react'
 import { toast } from 'sonner'
 import { useRouter } from 'next/router'
+import { registerUser } from '@/actions/auth.actions'
 
 const RegisterPage = () => {
   return ( 
